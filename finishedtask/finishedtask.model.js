a2892272d9ea66ad3359c685998bfd190a95d1ff
ArneVandePoel <r0655272@student.thomasmore.be>
2018-11-19 13:07:45 +0100

    task and finishedtask

---
+++ finishedtask/finishedtask.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    key: {type: String, required: true, unique: true},
+    explanation: {type: String, required: true},
+    date: {type: Date, required: true},
+    userKey: {type: String, required: true},
+    taskKey: {type: String, required: true}
+});
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('FinishedTask', schema);
