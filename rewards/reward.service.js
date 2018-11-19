8014758d8ada9ea1ec526dc034819df5e8050195
Stijn Geens <stijn.geens@outlook.be>
2018-11-19 17:40:28 +0100

    Add remaining methods to Reward Service and Controller

--- rewards/reward.service.js
+++ rewards/reward.service.js
@@ -2,9 +2,37 @@ const db = require('_helpers/db');
 const Reward = db.Reward;
 
 module.exports = {
-    getAll
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
 };
 
 async function getAll() {
     return await Reward.find().select('-hash');
+}
+
+async function getById(id) {
+    return await Reward.findById(id).select('-hash');
+}
+
+async function create(rewardParam) {
+    const reward = new Reward(rewardParam);
+
+    await reward.save();
+}
+
+async function update(id, rewardParam) {
+    const reward = await Reward.findById(id);
+
+    if (!reward) throw 'Reward not found';
+
+    Object.assign(reward, rewardParam);
+
+    await reward.save();
+}
+
+async function _delete(id) {
+    await Reward.findByIdAndRemove(id);
 }
--- rewards/rewards.controller.js
+++ rewards/rewards.controller.js
@@ -4,11 +4,39 @@ const rewardService = require('./reward.service');
 
 // routes
 router.get('/', getAll);
+router.get('/:id', getById);
+router.post('/', create);
+router.put('/:id', update);
+router.delete('/:id', _delete);
 
 module.exports = router;
 
 function getAll(req, res, next) {
     rewardService.getAll()
         .then(rewards => res.json(rewards))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    rewardService.getById(req.params.id)
+        .then(reward => reward ? res.json(reward) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function create(req, res, next) {
+    rewardService.create(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function update(req, res, next) {
+    rewardService.update(req.params.id, req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function _delete(req, res, next) {
+    rewardService.delete(req.params.id)
+        .then(() => res.json({}))
         .catch(err => next(err));
 }
