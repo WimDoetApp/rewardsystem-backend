736ee141c9d6efd60766afeaa00c9639ca26aba9
Wim Naudts <wimnaudts@yahoo.com>
2018-11-22 17:17:40 +0100

    process orders

--- rewards/reward.model.js
+++ rewards/reward.model.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
     name: { type: String, required: true },
-    points: { type: Number, required: true }
+    points: { type: Number, required: true },
+    imageUrl: { type: String, required: true }
 });
 
 schema.set('toJSON', { virtuals: true });
